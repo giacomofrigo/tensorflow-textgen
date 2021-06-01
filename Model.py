afec99d960a0aa05830b15650371cc3c9f56d4b0
giacomofrigo <giacomo.frigo98@gmail.com>
2021-06-01 16:02:18 +0200

    make multi layers configurable

--- Model.py
+++ Model.py
@@ -1,28 +1,30 @@
 import tensorflow as tf
 
 class Model(tf.keras.Model):
-  def __init__(self, vocab_size, embedding_dim, rnn_units):
+  def __init__(self, vocab_size, embedding_dim, rnn_units, n_layers = 2):
     super().__init__(self)
-    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-    self.gru = tf.keras.layers.GRU(rnn_units,
-                                   return_sequences=True,
-                                   return_state=True)
-    self.gru_2 = tf.keras.layers.GRU(rnn_units,
-                                   return_sequences=True,
-                                   return_state=True)
+    self.n_layers=n_layers
 
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+
+    self.rnn_layers = [tf.keras.layers.GRU(rnn_units,
+                                   return_sequences=True,
+                                   return_state=True) for layer in range (n_layers)]
 
     self.dense = tf.keras.layers.Dense(vocab_size)
 
   def call(self, inputs, states=None, return_state=False, training=False):
     x = inputs
     x = self.embedding(x, training=training)
-    if states is None:
-      states = [self.gru.get_initial_state(x), self.gru_2.get_initial_state(x)]
 
-    new_states=[None, None]
-    x, new_states[0] = self.gru(x, initial_state=states[0], training=training)
-    x, new_states[1] = self.gru(x, initial_state=states[1], training=training)
+    if states is None:
+      states = [self.rnn_layers[layer].get_initial_state(x) for layer in range(self.n_layers)]
+
+    new_states=[None for layer in range(self.n_layers)]
+
+    for layer_index, layer in enumerate(self.rnn_layers):
+      x, new_state = layer(x, initial_state=states[layer_index], training=training)
+      new_states[layer_index] = new_state
 
     x = self.dense(x, training=training)
 
